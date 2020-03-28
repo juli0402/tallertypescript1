a45adb34ff4b35d81d423fa67c7631415cadea1b
Juliana <mj.rojass1@uniandes.edu.co>
2020-03-28 18:52:49 -0500

    Taller completo xd

---
+++ scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de Sw", "Cesar Forero", 4),
+    new Course("Futbol 1", "Freddy Rincón", 2),
+    new Course("Arquitectura Empresarial", "María Camila Romero", 2),
+    new Course("Análisis de Decisión de Inversión", "Miguel Angel Garcia", 1),
+    new Course("Futbol 2", "James R", 6)
+];
